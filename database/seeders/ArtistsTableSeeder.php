84fb922021ee1457764bb4e9c5bbfc972454395d
Samuele-Benato <samuele.benato@gmail.com>
2023-10-18 16:00:46 +0200

    add seeders

---
+++ database/seeders/ArtistsTableSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ArtistsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
